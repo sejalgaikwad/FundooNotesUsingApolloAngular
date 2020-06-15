07242b486e57e0da065cb493d4489f68c936ed76
sejalgaikwad <sejalgaikwad24@gmail.com>
2020-06-15 12:47:01 +0530

    Added: routing in pages to render page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'forgotPassword',
+    component: ForgotPasswordComponent
+  },
+  {
+    path: 'resetPassword',
+    component: ResetPasswordComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
