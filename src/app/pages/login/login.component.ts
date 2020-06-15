1c85623095b183a935bbbc17774025f8e1e1d02a
sejalgaikwad <sejalgaikwad24@gmail.com>
2020-06-15 18:50:02 +0530

    Added: login form and validation using angular material

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -8,8 +10,17 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
-
+  email = new FormControl("", [Validators.required, Validators.email]);
+  password = new FormControl("", [Validators.required]);
+ 
   ngOnInit(): void {
   }
-
+  hide= true;
+  login(){
+    console.log(this.email.value);
+    console.log(this.password.value);
+  }
 }
+
+
+  
